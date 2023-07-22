0433f3a2618174c7d78f60b7fd01bba529f2c285
Shami Sharma <shamisharma01@outlook.com>
2023-07-22 19:55:40 -0400

    Stored message components in Arrays

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+//Component 1 of the Car Haiku
+const comp_one = ['Roaring engine hums,', 'Sunset on the road,', 'Windows down, breeze weaves,', 
+                  'Raindrops kiss the hood,','Gleaming chrome and steel,','Wheels turn, miles fly by,'];
+//Component 2 of the Car Haiku
+let comp_two = ['Metal steed glides through the night,','Tires spin, asphalt whispers,', 'Open highway, endless dreams,',
+                'Headlights cut through misty veil,', 'Silent guardian parked in dusk,','Passing landscapes like a dream,'];
+//Component 3 of the Car Haiku
+let comp_three = ['Freedom\'s embrace calls.', 'Adventure awaits.', 'Carry me away.', 'Journey guided home.',
+                  'Resting for the drive.', 'Passing landscapes like a dream,'];

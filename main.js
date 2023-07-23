3007fbbba51d4cfaafb64dcbf293dd8ce12c37fb
Shami Sharma <shamisharma01@outlook.com>
2023-07-22 20:26:08 -0400

    Added random_number and display_title function

--- main.js
+++ main.js
@@ -15,10 +15,14 @@ const display_title = () => {
     console.log('------------------------------------');
 }
 
+const get_random_num = num => {
+    return Math.floor(Math.random() * num);
+}
+
 const get_messages = () => {
-    const haiku_line_one = comp_one_arr[Math.floor(Math.random() * comp_one_arr.length)];
-    const haiku_line_two = comp_two_arr[Math.floor(Math.random() * comp_two_arr.length)];
-    const haiku_line_three = comp_three_arr[Math.floor(Math.random() * comp_three_arr.length)];
+    const haiku_line_one = comp_one_arr[get_random_num(comp_one_arr.length)];
+    const haiku_line_two = comp_two_arr[get_random_num(comp_two_arr.length)];
+    const haiku_line_three = comp_three_arr[get_random_num(comp_three_arr.length)];
 
     const final_haiku = `${haiku_line_one}\n${haiku_line_two}\n${haiku_line_three}`;
     console.log(final_haiku);
